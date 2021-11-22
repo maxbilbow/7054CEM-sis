d91eba3ad26206067e4f2fd8f96a27af2f8e908d
maxbilbow <maxbilbow@gmail.com>
2021-11-22 07:31:03 +0000

    Can now deploy web-app

--- sis-api/api.py
+++ sis-api/api.py
@@ -7,13 +7,13 @@ from core import config
 from swagger_server import encoder
 
 
-def main():
+def run():
     config.load_config(r"%s/config.yml" % dirname(__file__))
-    app = connexion.App(__name__, specification_dir='./swagger/')
+    app = connexion.App(__name__, specification_dir='./swagger_server/swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Smart Insurance System'}, pythonic_params=True)
-    app.run(port=8080)
+    app.run(port=config.get("port"))
 
 
 if __name__ == '__main__':
-    main()
+    run()
--- sis-web/rest/index.py
+++ sis-web/rest/index.py
@@ -1,14 +1,15 @@
-from flask import render_template, current_app as app
+from flask import render_template, send_file, send_from_directory, current_app as app
 
+import config
 from rest.decorators import login_required
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return send_from_directory(config.get("web_app.public"), "index.html")
 
 
-@app.route("/<ignored>")
+@app.route("/<path:filename>")
 @login_required
-def index_catch_all(ignored):
-    return index()
+def index_catch_all(filename: str):
+    return send_from_directory(config.get("web_app.public"), filename)
